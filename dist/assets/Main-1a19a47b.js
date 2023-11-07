572764472d8c911c841e3f2d929fe14a4d28c962
liy <1711467488@qq.com>
2023-11-07 23:21:37 +0800

    feat:

---
+++ dist/assets/Main-1a19a47b.js
@@ -0,0 +1,1 @@
+import{d as e,o as n,c as o}from"./index-2dd0d36e.js";const m=e({name:"DemoA",__name:"Main",setup(a){return(t,s)=>(n(),o("div",null," This is DemoA "))}});export{m as default};
---
+++ dist/assets/Main-dd33f735.js
@@ -0,0 +1,1 @@
+import{d as e,o as n,c as o}from"./index-2dd0d36e.js";const m=e({name:"DemoB",__name:"Main",setup(a){return(t,s)=>(n(),o("div",null," This is DemoB "))}});export{m as default};
